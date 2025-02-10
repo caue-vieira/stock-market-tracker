b88b58a1629c02841dd9b0fcf8402f68317672c2
unknown <cauevcaetano@gmail.com>
2025-02-10 09:54:54 -0300

    iniciando requisições para API Polygon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "./ui/input";
-import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./ui/navigation-menu";
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger, navigationMenuTriggerStyle } from "./ui/navigation-menu";
 import { Button } from "./ui/button";
 import { Moon, Search, Sun } from "lucide-react";
+import axios from "axios";
 
 function Navbar() {
+    // Melhorar dark mode
     const [darkMode, setDarkMode] = useState<boolean>(false);
     const [searchStocks, setSearchStocks] = useState<String>('');
+
+    useEffect(() => {
+        console.log(searchStocks);
+    }, [searchStocks]);
+
+    useEffect(() => {
+        axios.get(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2024-01-09/2024-02-10?adjusted=true&sort=asc&apiKey=${import.meta.env.VITE_POLYGON_KEY}`)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error.response.data);
+            })
+    }, []);
 
     return (
         <div className={`flex p-2 h-16 justify-between border-b-2 ${!darkMode ? 'shadow-md' : 'border-b-zinc-200'}`}>
             <div className="flex gap-4 h-full w-full">
                 <h1 className="my-auto font-bold text-2xl">Stocks Tracker</h1>
-                <div className="flex gap-2 rounded-lg border-[1px] pr-2 shadow-sm">
-                    <Input className="w-48 my-auto border-none shadow-none" placeholder="Escolha uma outra ação" />
+                <Button className="w-9 my-auto" variant={'ghost'} onClick={() => setDarkMode(!darkMode)}>
+                    {!darkMode ? <Sun /> : <Moon className="text-zinc-50" />}
+                </Button>
+                <div className={`flex gap-2 rounded-lg border-[1px] pr-2 shadow-sm ${darkMode ? "bg-zinc-600" : null}`}>
+                    <Input className={`w-48 my-auto border-none shadow-none ${darkMode ? "text-zinc-100 placeholder:text-zinc-100 placeholder:opacity-50" : "text-zinc-700 placeholder:text-zinc-700 placeholder:opacity-75"} focus-visible:ring-0`} placeholder="Pesquise uma ação" onChange={(e) => setSearchStocks(e.target.value)} />
                     <Button className="w-9 my-auto rounded-full" variant={"ghost"}>
                         <Search className={`${darkMode ? 'text-zinc-50' : null}`} />
                     </Button>
                 </div>
-                <Button className="w-9 my-auto" variant={'ghost'} onClick={() => setDarkMode(!darkMode)}>
-                    {!darkMode ? <Sun /> : <Moon className="text-zinc-50" />}
-                </Button>
-            </div>
-            <div className="w-full flex justify-center">
                 <NavigationMenu>
                     <NavigationMenuList>
                         <NavigationMenuItem>
@@ -37,8 +51,14 @@ function Navbar() {
                                 </ul>
                             </NavigationMenuContent>
                         </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink className={`hover:cursor-pointer ${navigationMenuTriggerStyle()}`}>Notícias</NavigationMenuLink>
+                        </NavigationMenuItem>
                     </NavigationMenuList>
                 </NavigationMenu>
+            </div>
+            <div className="w-full flex justify-center">
+                
             </div>
         </div>
     )
